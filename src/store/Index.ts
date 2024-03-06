89e747677bf7f68d778f3a00453231a07f6efae8
Partha_Sen <partha.s.sen@gmail.com>
2024-03-06 13:02:26 +0530

    persisting chanhes to browser local store

--- src/store/Index.ts
+++ src/store/Index.ts
@@ -1,14 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from "./user/User.slice"
+import { persistStore } from 'redux-persist';
 
 export const rootReducer = combineReducers({
     users: userReducer
 })
 
+const persistConfig = {
+    key: 'root',
+    storage,
+  };
+
+
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-    reducer: rootReducer
+    reducer: persistedReducer
 })
 
 export type RootState = ReturnType<typeof store.getState>;
+export const persistor = persistStore(store);
 export default store;
